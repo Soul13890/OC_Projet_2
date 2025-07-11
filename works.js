1300ce4a29257c1481c1aa9cb5f92c05715b5ec7
Florian Badenes <f.badenes@orange.fr>
2025-07-11 13:54:23 +0200

    Ajout des filtres pour afficher les travaux par catégories

--- works.js
+++ works.js
@@ -2,6 +2,10 @@
 const responseWork = await fetch('http://localhost:5678/api/works');   
 let works = await responseWork.json();
 
+//Récupération des catégories depuis l'API
+const responseCategories = await fetch('http://localhost:5678/api/categories');    
+let categories = await responseCategories.json();
+
 function generateWorks(works)
 {
     // Récupération de l'élément du DOM qui accueillera les fiches
@@ -29,3 +33,71 @@ function generateWorks(works)
 }
 
 generateWorks(works);
+
+// Gestion des boutons
+function generateFilters(works)
+{ 
+    // Création du bouton "Tous"
+    const buttonContainer = document.querySelector(".filters");
+    buttonContainer.innerHTML = "";
+    const buttonAllElement = document.createElement("button");
+    buttonAllElement.innerText = "Tous";    
+    buttonAllElement.classList.add("active");
+    buttonAllElement.addEventListener("click", function () {
+        // Vidage de la galerie
+        const gallery = document.querySelector(".gallery");
+        gallery.innerHTML = "";
+        // Enlever la classe active de tous les filtres
+        for(let i = 0; i < buttonContainer.children.length; i++)
+        {
+            if(buttonContainer.children[i].classList.contains("active"))
+            {
+                buttonContainer.children[i].classList.remove("active");
+            }
+        }
+        // Activer le bouton concerné
+        buttonAllElement.classList.add("active");
+        generateWorks(works);
+    });
+    buttonContainer.appendChild(buttonAllElement);
+
+    // Récupération des différentes catégories
+    const categoriesSet = new Set();
+    for(let i = 0; i < categories.length; i++)
+    {
+        if(!categoriesSet.has(categories[i].name))
+        {
+            categoriesSet.add(categories[i].name);
+        }
+    }
+
+    // Création d'un bouton de filtrage pour chaque catégories existantes
+    for(let item of categoriesSet)
+    {
+        const buttonTemp = document.createElement("button");
+        buttonTemp.innerText = item;        
+        buttonTemp.addEventListener("click", function () {
+            // Empty the gallery
+            const gallery = document.querySelector(".gallery");
+            gallery.innerHTML = "";
+            // Enlever la classe active de tous les filtres
+            for(let i = 0; i < buttonContainer.children.length; i++)
+            {
+                if(buttonContainer.children[i].classList.contains("active"))
+                {
+                    buttonContainer.children[i].classList.remove("active");
+                }
+            }
+            // Activer le bouton concerné
+            buttonTemp.classList.add("active");
+            // Repopulate the gallery
+            const worksFiltered = works.filter(function (work){
+            return work.category.name === item;           
+            });
+            generateWorks(worksFiltered);
+        });
+        buttonContainer.appendChild(buttonTemp);
+    }
+}
+
+generateFilters(works);
